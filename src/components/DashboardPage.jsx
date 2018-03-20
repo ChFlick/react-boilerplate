d3f8db3ef547e7a4bed52f6241748ffb1ec352d2
Christoph Flick <christophflick@gmx.de>
2018-03-20 21:10:52 +0100

    initial commit

---
+++ src/components/DashboardPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const DashboardPage = () => (
+    <div>
+        Dasboard page content
+    </div>
+);
+
+export default DashboardPage;
---
+++ src/store/configureStore.jsx
@@ -0,0 +1,13 @@
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+import authReducer from '../reducers/auth';
+
+const composedEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export default () => createStore(
+    combineReducers({
+        auth: authReducer
+    }),
+    composedEnhancers(applyMiddleware(thunk))
+);
